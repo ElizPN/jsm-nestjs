5b9f528eb15815b94bfa728d4b19e4dfe0407ac1
ElizPN <pankevych.liza@gmail.com>
2026-08-27 13:18:14 +0200

    feat: update greeting message in AppService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('hello')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    console.log(this.appService);
     return this.appService.getHello();
+    // is it true that this creates object with appService property and stores
+    // refference to object with appService property,
+
+    // Why creates instance of appService service?
+    // And most importannt when is it crateed?
+
+    // do we create insanse of appService in constructor or in 11 line esing this?
   }
 }
